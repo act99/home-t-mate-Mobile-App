4558604ce43e572855673e68781e5cbc70b83ae0
JOOSEOK LEE <dorxm999@gmail.com>
2022-04-11 19:10:35 +0900

    웹뷰 설정 및 타이틀 변경

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '홈트메이트',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
